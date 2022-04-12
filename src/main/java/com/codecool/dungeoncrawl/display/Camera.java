4a1a29572be4ded9d5f36ff1dd8fe4d48f841c64
suaemp <wiktoria.sladowska@gmail.com>
2022-04-12 21:57:41 +0200

    implemented setValidTargetField()

--- src/main/java/com/codecool/dungeoncrawl/display/Camera.java
+++ src/main/java/com/codecool/dungeoncrawl/display/Camera.java
@@ -8,14 +8,14 @@ public class Camera {
     private final int hRange;
     private final int vRange;
     private int xPos;
-    private int yPox;
+    private int yPos;
     private GameMap map;
     private int[] targetField;
     private CameraField[][] matrixInView;
 
     public Camera(GameMap map, int[] targetField, int hRange, int vRange){
         this.map = map;
-        this.targetField = targetField;
+        this.targetField = targetField; // od 1
         this.hRange = hRange;
         this.vRange = vRange;
         setValidTargetField();
@@ -23,7 +23,21 @@ public class Camera {
     }
 
     private void setValidTargetField(){
+        int hMinDistance = (hRange - 1) / 2;
+        int vMinDistance = (vRange - 1) / 2;
+        int hDistToMapBorder = map.getWidth() - targetField[0];
+        int vDistToMapBorder = map.getHeight() - targetField[1];
 
+        if (targetField[0] <= hMinDistance){
+            targetField[0] = hMinDistance + 1;
+        } else if (hDistToMapBorder <= hMinDistance){
+            targetField[0] = map.getWidth() - hMinDistance - 1;
+        }
+        if (targetField[1] <= vMinDistance){
+            targetField[1] = vMinDistance + 1;
+        } else if (vDistToMapBorder <= vMinDistance){
+            targetField[1] = map.getHeight() - vMinDistance - 1;
+        }
     }
 
     private void initMatrixInView(){
